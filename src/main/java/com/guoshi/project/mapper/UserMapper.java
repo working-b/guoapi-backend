d38476d1c2b99c1639632fae45ee191a24df0c38
han.zt <han.zt@asiainfo-sec.com>
2024-05-30 18:14:15 +0800

    reset

---
+++ src/main/java/com/guoshi/project/mapper/UserMapper.java
@@ -0,0 +1,15 @@
+package com.guoshi.project.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.guoshi.project.model.entity.User;
+
+/**
+ * @Entity com.guoapi.project.model.domain.User
+ */
+public interface UserMapper extends BaseMapper<User> {
+
+}
+
+
+
+
